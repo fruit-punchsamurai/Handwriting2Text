e42cde80b4e83db56fadb7be0522d1a1851f589e
Nimesh Gopal Pradhan <nimeshgpradhan@gmail.com>
2025-04-27 09:12:21 +0545

    added runtime.txt

--- app.py
+++ app.py
@@ -317,4 +317,5 @@ def process_image():
 
 if __name__ == '__main__':
     os.makedirs('temp', exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host='0.0.0.0', port=port, debug=True)
